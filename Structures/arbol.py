e96e120b111006b4565afecd1bb142a062f6a293
jdchavarria <jdchavarria21@gmail.com>
2019-10-19 19:56:18 -0600

    actualizando

---
+++ Structures/arbol.py
@@ -0,0 +1,14 @@
+
+
+
+class NodoAvl:
+	def __init__(self, dat):
+		self.dato = dat
+		self.hijoIzquierdo = None
+		self.hijoDerecho = None
+
+class ArbolAvl:
+	def __init__(self):
+		raiz = None
+
+	
